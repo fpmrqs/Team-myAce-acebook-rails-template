47190d8a77ddabb3f1c24cedd1b2c97eb8e0c5f4
Francisco Pottinger-Marques <fpottinger.marques@gmail.com>
2022-02-18 07:16:34 +0000

    added rescue - debug aws

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  rescue_from ActionController::InvalidAuthenticityToken do |exception|
+    render current_user_path # Example method that will destroy the user cookies
+  end
   
   def authenticate_user(current_user)!
     if user_signed_in?
