81898ed01b655504ca088400395e10de3c02576a
Francisco Pottinger-Marques <fpottinger.marques@gmail.com>
2022-02-18 10:10:18 +0000

    added default avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,14 @@ class User < ApplicationRecord
   has_one :profile, dependent: :destroy
   has_many :posts, dependent: :destroy
 
+  def check_avatar
+    if self.avatar.attached?
+      self.avatar
+    else
+      "/default_avatar.png"
+    end
+  end
+
   has_one_attached :avatar
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
